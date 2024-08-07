82130d7623132448097f3e6bb83d3f74c301bd02
Norbert Grover <norbert298@gmail.com>
2024-08-07 14:22:11 -0700

    Initial Commit

---
+++ SearchTVShowsWithCombine/SearchTVShowsWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTVShowsWithCombineApp.swift
+//  SearchTVShowsWithCombine
+//
+//  Created by Norbert Grover on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchTVShowsWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
